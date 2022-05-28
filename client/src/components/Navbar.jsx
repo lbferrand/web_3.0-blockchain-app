906e9a80f515036c62e20ace6cb726e5fecea56a
lbferrand <lbferrand@gmail.com>
2022-05-28 18:30:00 -0400

    Install react-icons and ethers package json

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,12 @@
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiAulineClose } from "react-icons/ai";
+
+import logo from "../../images/logo.png";
+
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4"></nav>
+  );
 };
 
 export default Navbar;
